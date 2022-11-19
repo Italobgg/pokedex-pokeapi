60ddd5faf0c031777f93f203b1c101bbce3bab27
italo <italobartolomeugarcia@gmail.com>
2022-11-19 18:50:35 -0300

    pokeapi

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [pokemons, setPokemons] = useState([]);
   const [favorites, setFavorites] = useState([]);
 
-  const itensPerPage = 25;
+  const itensPerPage = 24;
   const fetchPokemons = async () => {
     try {
       setLoading(true);
